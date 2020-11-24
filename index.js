e185b9e24925949d80a4f1364a3084fe51289842
Jana Zrayka <janjoune.97@hotmail.com>
2020-11-24 17:39:48 +0000

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 
 // Write your code here
+//1 
+let num1 =31;
+let num2 =2;
+let multiply =num1 * num2 ;
+
+//2
+let random = Math.random();
+
+//3 
+const x = 14;
+const y = 5;
+const mod = x % y;
+
+//4 
+
+let max = 20 ;

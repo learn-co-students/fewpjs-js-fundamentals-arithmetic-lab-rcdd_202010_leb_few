1a127428cb64abf98978ca6f0132ea04e6836471
Hiba Abdel karim <hibaabdelkarim2000@gmail.com>
2020-11-24 21:36:00 +0000

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+let multiply = num1*num2;
+let random = Math.random();
+
+let mod = 20%16;
+let set = [20,3,2,1];
+let max = set[0];
+for(let i = 0 ; i < set.length ; i++){
+  if(set[i] > max){
+    max = set[i];
+  }
+}

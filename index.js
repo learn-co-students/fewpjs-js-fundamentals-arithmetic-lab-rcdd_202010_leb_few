fcf9b5befd1de2bdde3ddb8accdef56c85df7116
Abdulrahman Ghalayini <ghalayini.abdulrahman.17@gmail.com>
2020-11-25 07:30:30 +0000

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,26 @@
 
-// Write your code here
+let random = Math.floor(Math.random() * 16) + 5;
+let num1=1;
+let num2= 62;
+function  mul (num1 , num2) {
+  if(num1*num2==62)
+  return 62;
+  return false;
+}
+let multiply = mul(num1, num2);
+let mod = 24%5;
+
+let num3=24;
+let num4= 5;
+let arr =[2,5 ,8,6,9,20];
+
+// function  maxi (arr) {
+//   let max=0;
+// for(i=0; i<arr.length; i++){
+// if(max<arr[i])
+// max=arr[i];
+// }
+// return max;
+// }
+let max = Math.max(2,5,9,20,19);
+console.log(max);
